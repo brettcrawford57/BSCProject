62b824806b07a0a98beb509296dde79f6560d17b
Brett Crawford <brett.crawford57@gmail.com>
2021-07-21 18:53:03 -0500

    started project: index starter

---
+++ src/test/java/com/bscgroup/bsc/BscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bscgroup.bsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
